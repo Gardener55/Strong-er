a3fb348ad645f9dfe8a166f3bbb72ebf303948ab
Chad Granofsky <chad.granofsky@gmail.com>
2025-08-08 21:45:07 -0400

    updates

---
+++ Strong-er/checklist.swift
@@ -0,0 +1,17 @@
+//
+//  checklist.swift
+//  Strong-er
+//
+//  Created by Evan Cohen on 8/8/25.
+//
+
+//  Additional workouts
+//  Workout video demos
+//  Charts
+//  Weight conversation lb/kg
+//  Remove "items" from settings
+//  Optimization
+//  Import csv data from other workouts
+//  History calendar
+//  Link to Apple Health, etc
+//  
